9ca56491874579b2cf37b4d6dde89539b2b3c9fa
Ubuntu <ubuntu@ip-172-31-16-74.us-west-2.compute.internal>
2017-11-07 15:42:05 +0000

    First Init

---
+++ main.php
@@ -0,0 +1,47 @@
+<?php
+DEFINE ("MIN", 1);
+DEFINE ("MAX", 13);
+
+//returns temporary returns on choosing high or low
+function tr (){
+
+        /*if ($nc = 1){
+        echo $r1;
+        $rt = $r1;
+   }
+        else {
+        echo $r2;
+        $rt = $r2;
+   }*/
+   $r1 = rand(MIN, MAX);
+   $r2 = rand(MIN, MAX);
+   $nc = rand (0,1);
+
+   return ($nc==1 ? $r1 : $r2 );
+}
+function hol(){
+
+
+   //echo ($nc==1 : r1 ? r2 );
+   $t1=tr();
+   $t2=tr();
+   echo $t2."\n"." Debug statment";
+   echo "The number generated : ".$t1 ."\n";
+   $n1 = readline("High Or Low?");
+	
+   echo "\n";
+   if ($n1 == "High" ) {
+
+        echo ($t1>$t2? "You Win" : "You Lose");
+		echo "\n";
+   }
+   elseif ($n1 == "Lose") {
+        echo ($t1<$t2? "You Win" : "You Lose");
+		echo "\n";
+   }
+   else {echo "Error : Please enter valid number dipshit";}
+
+}
+
+hol();
+?>
